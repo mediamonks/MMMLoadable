85cf64a01b5c559cc87f9c34cfdb61a551eab10f
Aleh Dzenisiuk <aleh@mediamonks.com>
2020-09-19 20:01:59 +0200

    MMMLoadableSyncer: allow to skip refreshes after success, support sync policies

--- Sources/MMMLoadable/MMMLoadableSyncer.swift
+++ Sources/MMMLoadable/MMMLoadableSyncer.swift
@@ -14,12 +14,19 @@ public final class MMMLoadableSyncer {
 
 	private weak var loadable: MMMLoadableProtocol?
 	private var loadableObserver: MMMLoadableObserver?
+	private let timeoutPolicy: MMMTimeoutPolicy
 
-	private let timeoutPolicy: TimeoutPolicy
+	/// Should the sync be triggered in any case ("hard" sync by calling `sync`) or only wnen needed (`syncIfNeeded`).
+	public enum SyncPolicy {
+		case sync
+		case syncIfNeeded
+	}
+	private let syncPolicy: SyncPolicy
 
 	/// Designated initializer allowing to customize the timeout policy, something that can be useful at least for testing.
-	public init(loadable: MMMLoadableProtocol, timeoutPolicy: TimeoutPolicy) {
+	public init(loadable: MMMLoadableProtocol, syncPolicy: SyncPolicy = .sync, timeoutPolicy: MMMTimeoutPolicy) {
 		self.loadable = loadable
+		self.syncPolicy = syncPolicy
 		self.timeoutPolicy = timeoutPolicy
 		self.loadableObserver = MMMLoadableObserver(loadable: loadable) { [weak self] _ in
 			self?.reschedule()
@@ -27,25 +34,34 @@ public final class MMMLoadableSyncer {
 		reschedule()
 	}
 
-	deinit {
-    	cancelTimer()
-	}
+	/// Describes how often to retry syncing the target after a failure and how this timeout should grow after each attempt.
+	public typealias BackoffSettings = (min: TimeInterval, max: TimeInterval, multiplier: Double)
 
-	/// Convenience for syncing a loadable at regular intervals using a simple back off policy in case of failures.
+	/// Sets up to sync a loadable at regular intervals using a simple back off policy in case of failures.
+	///
+	/// - Parameter period: How often to sync the target after it has been synced successfully. 0 to disable.
+	/// - Parameter backoff: Describes how often to retry syncing the target after a failure and how this timeout
+	///   should grow after each attempt.
 	public convenience init(
 		loadable: MMMLoadableProtocol,
+		syncPolicy: SyncPolicy = .sync,
 		period: TimeInterval,
-		backoff: (min: TimeInterval, max: TimeInterval, multiplier: Double)
+		backoff: BackoffSettings
 	) {
 		self.init(
 			loadable: loadable,
-			timeoutPolicy: BackoffTimeoutPolicy(
+			syncPolicy: syncPolicy,
+			timeoutPolicy: MMMBackoffTimeoutPolicy(
 				period: period,
 				min: backoff.min,
 				max: backoff.max,
 				multiplier: backoff.multiplier
 			)
 		)
+	}
+
+	deinit {
+    	cancelTimer()
 	}
 
 	private var timer: Timer?
@@ -57,7 +73,7 @@ public final class MMMLoadableSyncer {
 
 	private func setTimer(timeout: TimeInterval) {
 
-		let t = max(timeout, 0.1)
+		let t = max(timeout, 0)
 		timer?.invalidate()
 		timer = Timer.scheduledTimer(withTimeInterval: t, repeats: false) { [weak self] _ in
 			self?.sync()
@@ -68,17 +84,28 @@ public final class MMMLoadableSyncer {
 	}
 
 	private func sync() {
+
 		cancelTimer()
-		// TODO: Might be useful to do syncIfNeeded() in some cases. Make configurable?
-		loadable?.sync()
+
+		// The target can be gone anytime by design.
+		guard let loadable = loadable else { return }
+
+		switch syncPolicy {
+		case .sync:
+			loadable.sync()
+		case .syncIfNeeded:
+			loadable.syncIfNeeded()
+			if loadable.loadableState == .didSyncSuccessfully {
+				// Looks like no sync was required, so most likely no state change occurred and triggered `reschedule()`.
+				reschedule()
+			}
+		}
 	}
 
 	private func reschedule() {
 
-		guard let loadable = loadable else {
-			// It's weak by design, so might be gone and then we just stop refreshing it.
-			return
-		}
+		// The target can be gone anytime by design.
+		guard let loadable = loadable else { return }
 
 		switch loadable.loadableState {
 		case .idle:
@@ -90,20 +117,26 @@ public final class MMMLoadableSyncer {
 		case .didFailToSync:
 			setTimer(timeout: timeoutPolicy.nextTimeout(afterFailure: true))
 		case .didSyncSuccessfully:
-			setTimer(timeout: timeoutPolicy.nextTimeout(afterFailure: false))
+			let timeout = timeoutPolicy.nextTimeout(afterFailure: false)
+			if timeout > 0 {
+				setTimer(timeout: timeout)
+			} else {
+				// Treating 0 as "no sync after success required".
+			}
 		}
 	}
 }
 
 /// Tells when to try doing something next time depending on the outcome of the previous attempt (succeeded/failed)
 /// and possibly external factors.
-public protocol TimeoutPolicy: AnyObject {
+public protocol MMMTimeoutPolicy: AnyObject {
+	/// A 0 after a success is treated as "no action required".
 	func nextTimeout(afterFailure: Bool) -> TimeInterval
 }
 
 /// A timeout policy that is using a constant period after successful attempts and a set of incrementing timeouts
 /// after failures.
-public final class BackoffTimeoutPolicy: TimeoutPolicy {
+public final class MMMBackoffTimeoutPolicy: MMMTimeoutPolicy {
 
 	private let period: TimeInterval
 	private let min: TimeInterval

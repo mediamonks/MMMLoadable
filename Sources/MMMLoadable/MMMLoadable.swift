1c6c4da637a9bd1e98e85b065acec6ab399d3ece
Erik Terwan <erik.terwan@mediamonks.com>
2022-03-28 16:23:34 +0200

    Add `sink` method to quickly attach a MMMLoadableObserver
    So it now passes the actual loadable subclass instead of a generic
    MMMPureLoadableProtocol inside the closure.

--- Sources/MMMLoadable/MMMLoadable.swift
+++ Sources/MMMLoadable/MMMLoadable.swift
@@ -13,3 +13,18 @@ extension MMMLoadableState: CustomDebugStringConvertible {
 	
 	public var debugDescription: String { NSStringFromMMMLoadableState(self) }
 }
+
+extension MMMPureLoadableProtocol {
+    
+    /// Observe changes in this loadable. Will stop listening to changes when
+    /// ``MMMLoadableObserver/remove()`` is called or the observer deallocates.
+    /// 
+    /// - Parameter block: Get's called every time the loadable changes.
+    /// - Returns: The observer, you usually want to store this outside of the scope, e.g.
+    ///            in a private property so it doesn't deallocate right away.
+    public func sink(_ block: @escaping (Self) -> Void) -> MMMLoadableObserver? {
+        return MMMLoadableObserver(loadable: self) { loadable in
+            block(loadable as! Self)
+        }
+    }
+}
